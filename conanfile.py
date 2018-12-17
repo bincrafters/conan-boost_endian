f74561b0992fff50128506ade4b93abe150efb5e
Rene Rivera <grafikrobot@gmail.com>
2018-12-17 14:02:38 -0600

    Initial 1.69.0 packages. [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.68.0@bincrafters/testing")
+base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostEndianConan(base.BoostBaseConan):
     name = "boost_endian"
@@ -17,7 +17,6 @@ class BoostEndianConan(base.BoostBaseConan):
         "boost_core",
         "boost_predef",
         "boost_static_assert",
-        "boost_system",
         "boost_type_traits",
         "boost_utility"
     ]
